885a1afaac2bd6c484af0aeb52b6e3796dfbaa0f
Adelaida Avila <105053313+up-in-the-clouds@users.noreply.github.com>
2024-11-19 18:08:05 +0100

    upload html css assets

---
+++ script.js
@@ -0,0 +1,268 @@
+function myFunction() {
+  var dots = document.getElementById("dots");
+  var moreText = document.getElementById("more");
+  var btnText = document.getElementById("myBtn");
+
+  if (dots.style.display === "none") {
+    dots.style.display = "inline";
+    btnText.innerHTML = "+";
+    moreText.style.display = "none";
+  } else {
+    dots.style.display = "none";
+    btnText.innerHTML = "-";
+    moreText.style.display = "inline";
+  }
+}
+function myFunction1() {
+  var dots1 = document.getElementById("dots1");
+  var moreText1 = document.getElementById("more1");
+  var btnText1 = document.getElementById("myBtn1");
+
+  if (dots1.style.display === "none") {
+    dots1.style.display = "inline";
+    btnText1.innerHTML = "+";
+    moreText1.style.display = "none";
+  } else {
+    dots1.style.display = "none";
+    btnText1.innerHTML = "-";
+    moreText1.style.display = "inline";
+  }
+}
+function myFunction2() {
+  var dots2 = document.getElementById("dots2");
+  var moreText2 = document.getElementById("more2");
+  var btnText2 = document.getElementById("myBtn2");
+
+  if (dots2.style.display === "none") {
+    dots2.style.display = "inline";
+    btnText2.innerHTML = "+";
+    moreText2.style.display = "none";
+  } else {
+    dots2.style.display = "none";
+    btnText2.innerHTML = "-";
+    moreText2.style.display = "inline";
+  }
+}
+function myFunction3() {
+  var dots3 = document.getElementById("dots3");
+  var moreText3 = document.getElementById("more3");
+  var btnText3 = document.getElementById("myBtn3");
+
+  if (dots3.style.display === "none") {
+    dots3.style.display = "inline";
+    btnText3.innerHTML = "+";
+    moreText3.style.display = "none";
+  } else {
+    dots3.style.display = "none";
+    btnText3.innerHTML = "-";
+    moreText3.style.display = "inline";
+  }
+}
+function myFunction4() {
+  var dots4 = document.getElementById("dots4");
+  var moreText4 = document.getElementById("more4");
+  var btnText4 = document.getElementById("myBtn4");
+
+  if (dots4.style.display === "none") {
+    dots4.style.display = "inline";
+    btnText4.innerHTML = "+";
+    moreText4.style.display = "none";
+  } else {
+    dots4.style.display = "none";
+    btnText4.innerHTML = "-";
+    moreText4.style.display = "inline";
+  }
+}
+function myFunction5() {
+  var dots5 = document.getElementById("dots5");
+  var moreText5 = document.getElementById("more5");
+  var btnText5 = document.getElementById("myBtn5");
+
+  if (dots5.style.display === "none") {
+    dots5.style.display = "inline";
+    btnText5.innerHTML = "+";
+    moreText5.style.display = "none";
+  } else {
+    dots5.style.display = "none";
+    btnText5.innerHTML = "-";
+    moreText5.style.display = "inline";
+  }
+}
+function myFunction6() {
+  var dots6 = document.getElementById("dots6");
+  var moreText6 = document.getElementById("more6");
+  var btnText6 = document.getElementById("myBtn6");
+
+  if (dots6.style.display === "none") {
+    dots6.style.display = "inline";
+    btnText6.innerHTML = "+";
+    moreText6.style.display = "none";
+  } else {
+    dots6.style.display = "none";
+    btnText6.innerHTML = "-";
+    moreText6.style.display = "inline";
+  }
+}
+function myFunction7() {
+  var dots7 = document.getElementById("dots7");
+  var moreText7 = document.getElementById("more7");
+  var btnText7 = document.getElementById("myBtn7");
+
+  if (dots7.style.display === "none") {
+    dots7.style.display = "inline";
+    btnText7.innerHTML = "+";
+    moreText7.style.display = "none";
+  } else {
+    dots7.style.display = "none";
+    btnText7.innerHTML = "-";
+    moreText7.style.display = "inline";
+  }
+}
+function myFunction8() {
+  var dots8 = document.getElementById("dots8");
+  var moreText8 = document.getElementById("more8");
+  var btnText8 = document.getElementById("myBtn8");
+
+  if (dots8.style.display === "none") {
+    dots8.style.display = "inline";
+    btnText8.innerHTML = "+";
+    moreText8.style.display = "none";
+  } else {
+    dots8.style.display = "none";
+    btnText8.innerHTML = "-";
+    moreText8.style.display = "inline";
+  }
+}
+function myFunction9() {
+  var dots9 = document.getElementById("dots9");
+  var moreText9 = document.getElementById("more9");
+  var btnText9 = document.getElementById("myBtn9");
+
+  if (dots9.style.display === "none") {
+    dots9.style.display = "inline";
+    btnText9.innerHTML = "+";
+    moreText9.style.display = "none";
+  } else {
+    dots9.style.display = "none";
+    btnText9.innerHTML = "-";
+    moreText9.style.display = "inline";
+  }
+}
+function myFunction10() {
+  var dots10 = document.getElementById("dots10");
+  var moreText10 = document.getElementById("more10");
+  var btnText10 = document.getElementById("myBtn10");
+
+  if (dots10.style.display === "none") {
+    dots10.style.display = "inline";
+    btnText10.innerHTML = "+";
+    moreText10.style.display = "none";
+  } else {
+    dots10.style.display = "none";
+    btnText10.innerHTML = "-";
+    moreText10.style.display = "inline";
+  }
+}
+
+function myFunction11() {
+  var dots11 = document.getElementById("dots11");
+  var moreText11 = document.getElementById("more11");
+  var btnText11 = document.getElementById("myBtn11");
+
+  if (dots11.style.display === "none") {
+    dots11.style.display = "inline";
+    btnText11.innerHTML = "+";
+    moreText11.style.display = "none";
+  } else {
+    dots11.style.display = "none";
+    btnText11.innerHTML = "-";
+    moreText11.style.display = "inline";
+  }
+}
+function myFunction12() {
+  var dots12 = document.getElementById("dots12");
+  var moreText12 = document.getElementById("more12");
+  var btnText12 = document.getElementById("myBtn12");
+
+  if (dots12.style.display === "none") {
+    dots12.style.display = "inline";
+    btnText12.innerHTML = "+";
+    moreText12.style.display = "none";
+  } else {
+    dots12.style.display = "none";
+    btnText12.innerHTML = "-";
+    moreText12.style.display = "inline";
+  }
+}
+
+function myFunction13() {
+  var dots13 = document.getElementById("dots13");
+  var moreText13 = document.getElementById("more13");
+  var btnText13 = document.getElementById("myBtn13");
+
+  if (dots13.style.display === "none") {
+    dots13.style.display = "inline";
+    btnText13.innerHTML = "+";
+    moreText13.style.display = "none";
+  } else {
+    dots13.style.display = "none";
+    btnText13.innerHTML = "-";
+    moreText13.style.display = "inline";
+  }
+}
+
+function myFunction14() {
+  var dots14 = document.getElementById("dots14");
+  var moreText14 = document.getElementById("more14");
+  var btnText14 = document.getElementById("myBtn14");
+
+  if (dots14.style.display === "none") {
+    dots14.style.display = "inline";
+    btnText14.innerHTML = "+";
+    moreText14.style.display = "none";
+  } else {
+    dots14.style.display = "none";
+    btnText14.innerHTML = "-";
+    moreText14.style.display = "inline";
+  }
+}
+function myFunction15() {
+  var dots15 = document.getElementById("dots15");
+  var moreText15 = document.getElementById("more15");
+  var btnText15 = document.getElementById("myBtn15");
+
+  if (dots15.style.display === "none") {
+    dots15.style.display = "inline";
+    btnText15.innerHTML = "+";
+    moreText15.style.display = "none";
+  } else {
+    dots15.style.display = "none";
+    btnText15.innerHTML = "-";
+    moreText15.style.display = "inline";
+  }
+}
+
+function playVideo() {
+  var bgvideo = document.getElementById("myVideo");
+  bgvideo.muted = true;
+  bgvideo.play();
+  console.log("OK");
+}
+playVideo();
+
+function ifSafari() {
+  if (navigator.userAgent.toLowerCase().indexOf("safari/") > -1) {
+    console.log("True");
+    //show image
+    document.getElementById("myVideo").display = "none";
+    document.getElementById("myImage").display = "block";
+  } else {
+    // document.getElementById("a").display = "none"
+    //show video
+    console.log("False");
+    document.getElementById("myImage").display = "none";
+    document.getElementById("myVideo").display = "block";
+  }
+}
+
+ifSafari();
